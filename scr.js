645e953d12a2317a72efa01979ce40c71798af4f
Toe Khaing Oo <toekhaing.tko@gmail.com>
2024-12-17 17:49:56 +0700

    Update scr.js
    add bg color change

--- scr.js
+++ scr.js
@@ -1,12 +1,12 @@
 // Example XSS payload: Display an alert box
-alert("This is XSS";
+alert("This is XSS");
 
-
-document.body.innerHTML = `
-  <h1 style="color: red; text-align: center;">
-    XSS Attack Demonstration
-  </h1>
-  <p style="text-align: center;">
-    This content was injected via an XSS payload.
-  </p>
-`;
+document.body.style.backgroundColor = getRandomColor();
+function getRandomColor() {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+  }
