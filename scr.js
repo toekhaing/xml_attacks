f21d64134f8c7d8f31e0f627ff992b8d386fadd1
Toe Khaing Oo <toekhaing.tko@gmail.com>
2024-12-17 18:04:30 +0700

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,13 +1,1 @@
-// Example XSS payload: Display an alert box
 alert("This is XSS");
-function changeBackgroundColor() {
-      document.body.style.backgroundColor = getRandomColor();
-}
-function getRandomColor() {
-      const letters = '0123456789ABCDEF';
-      let color = '#';
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-}
